bba42222752987e1c3123c256aa45cfa05717634
hiecho <hioo520@163.com>
2024-04-12 21:57:32 +0800

    update

--- exercises/quiz3.rs
+++ exercises/quiz3.rs
@@ -16,15 +16,14 @@
 //
 // Execute `rustlings hint quiz3` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
 
-pub struct ReportCard {
-    pub grade: f32,
+pub struct ReportCard<T> {
+    pub grade: T,
     pub student_name: String,
     pub student_age: u8,
 }
 
-impl ReportCard {
+impl<T:std::fmt::Display> ReportCard<T> {
     pub fn print(&self) -> String {
         format!("{} ({}) - achieved a grade of {}",
             &self.student_name, &self.student_age, &self.grade)
