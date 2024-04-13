69c3c1a452226372e077bde0e5757e2971045acd
hiecho <hioo520@163.com>
2024-04-13 16:01:07 +0800

    update

--- exercises/tests/tests7.rs
+++ exercises/tests/tests7.rs
@@ -47,7 +47,7 @@ mod tests {
             .duration_since(std::time::UNIX_EPOCH)
             .unwrap()
             .as_secs();
-        let s = std::env::var("TEST_FOO").unwrap_or(timestamp.to_string());
+        let s = std::env::var("TEST_FOO").unwrap();
         let e: u64 = s.parse().unwrap();
         assert!(timestamp >= e && timestamp < e + 10);
     }
--- exercises/tests/tests8.rs
+++ exercises/tests/tests8.rs
@@ -7,7 +7,6 @@
 // Execute `rustlings hint tests8` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
 
 fn main() {}
 
@@ -16,7 +15,6 @@ mod tests {
     use super::*;
 
     #[test]
-    #[should_panic]
     fn test_success() {
         #[cfg(feature = "pass")]
         return;
